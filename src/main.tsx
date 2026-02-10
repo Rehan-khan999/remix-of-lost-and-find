e5442ca80b3ac8157d52d6fe366bfe209ec42138
Rehan-khan999 <rehanadilk@gmail.com>
2026-02-10 12:25:44 +0530

    Register service worker for Push API support

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import './i18n'
 import { ThemeProvider } from 'next-themes'
 import { BrowserRouter } from 'react-router-dom'
-//import { registerSW } from 'virtual:pwa-register'
+import { registerSW } from 'virtual:pwa-register'
 
 // Ensure the service worker is registered so Push API works
 registerSW({ immediate: true })
